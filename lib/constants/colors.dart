3b6ebe00e9e555d8c9eb1bb178b370626c01357c
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2025-12-30 07:59:43 -0500

    feat(onbording): add screens: onbording, widgets: app_button and onboarding_steps

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6366F1);
+  static const Color secondary = Color(0xFF8B5CF6);
+  static const Color white = Colors.white;
+  static const Color transparent = Colors.transparent;
+
+  static const double disabledOpacity = 0.5;
+  static const double inactiveOpacity = 0.3;
+}
