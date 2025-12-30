1d639e0b50fdb5c5a553f3b48d09886ea43e0153
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2025-12-30 09:23:30 -0500

    refactor(general): rename constants files to *_const.dart format

---
+++ lib/constants/subscription_const.dart
@@ -0,0 +1,25 @@
+import 'package:app/models/subscription.dart';
+
+class SubscriptionConstants {
+  static const String monthPrice = '299 ₽';
+  static const String yearPrice = '1999 ₽';
+  static const int yearDiscount = 44;
+
+  static String getPrice(SubscriptionType type) {
+    switch (type) {
+      case SubscriptionType.month:
+        return monthPrice;
+      case SubscriptionType.year:
+        return yearPrice;
+    }
+  }
+
+  static int getDiscount(SubscriptionType type) {
+    switch (type) {
+      case SubscriptionType.month:
+        return 0;
+      case SubscriptionType.year:
+        return yearDiscount;
+    }
+  }
+}
